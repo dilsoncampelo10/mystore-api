8c3327a6fc951dd5e8f6858d4889bbf08df0ccc7
dilsoncampelo10 <dilsono316@gmail.com>
2023-05-23 12:26:50 -0300

    resolve bugs annotations

--- src/main/java/com/dilson/mystore/controllers/ProductController.java
+++ src/main/java/com/dilson/mystore/controllers/ProductController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dilson.mystore.models.Product;
 import com.dilson.mystore.services.ProductService;
 
+@RestController
 @RequestMapping("/products")
 public class ProductController {
 
--- src/main/java/com/dilson/mystore/services/ProductService.java
+++ src/main/java/com/dilson/mystore/services/ProductService.java
@@ -3,11 +3,13 @@ package com.dilson.mystore.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dilson.mystore.models.Product;
 import com.dilson.mystore.repositories.ProductRepository;
 
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
